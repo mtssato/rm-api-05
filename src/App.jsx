69fe7935852a1561e222fda5e12e287a92267f32
sato <matheussato1702@gmail.com>
2026-06-02 11:12:26 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import s from './App.module.css'
+import { useEffect, useState } from 'react'
+import { api } from './constants/api'
 
-import './App.module.css'
+import logo from '/logo.png'
 
 function App() {
+  const [data, setData] = useState([])
+  const [page, setPage] = useState()
+
+  useEffect(() => {
+    api.get(`/character`).then((response) => {
+        setData(response.data.results)
+    }).catch((error) => {
+      console.error("Deu ruim!!!", error)
+    })
+  }, [page])
+  
 
   return (
     <>
-        
+    <div>
+      <label>Digite uma página</label>
+      <input type="number" placeholder='1/42' value={page} onChange={(e) => setPage(e.target.value)} /> 
+    </div>
+      <img className={s.logo} src={logo} alt="Logo" />
+      <main>
+        {data.map((item, index) => {
+          return(
+            <div key={index}>
+                <img src={item.image} alt={item.name} />
+                <h4>Name: {item.name}</h4>
+                <p>Species: {item.species}</p>
+                {item.status === "Dead" ? "Status:🧟" : item.status === "Alive" ? "Status: 😊" : <p>Status: {item.status}</p>}
+                <p>Origin: {item.origin.name}</p>
+            </div>
+          )
+        })}
+      </main>
     </>
   )
 }
